451181764e59983812ba5f8f20d71fb679c42891
iLegendTz <miguel.isaac@live.com>
2021-11-24 06:07:48 -0600

    Se corrigio el sistema de rutas.

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -7,7 +7,7 @@ export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PublicRoute />} />
+        <Route path="/*" element={<PublicRoute />} />
       </Routes>
     </BrowserRouter>
   );
