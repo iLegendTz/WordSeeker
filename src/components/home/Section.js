563aadaaff2535c74edc0ce5b5850c278fee4d1d
iLegendTz <miguel.isaac@live.com>
2021-11-24 06:07:02 -0600

    Se agregaron animaciones a los componentes.

--- src/components/home/Section.js
+++ src/components/home/Section.js
@@ -4,17 +4,21 @@ export const Section = ({ title, paragraphs, imgURL }) => {
   return (
     <div className="section__container row">
       <div className="col-md-6">
-        <h2 className="display-4 mb-3">
+        <h2 className="display-4 mb-3 animate__animated animate__fadeInDown">
           <b>{title}</b>
         </h2>
         {paragraphs.map((paragraph, key) => {
-          return <p key={key}>{paragraph}</p>;
+          return (
+            <p className="animate__animated animate__fadeInLeft" key={key}>
+              {paragraph}
+            </p>
+          );
         })}
       </div>
 
       <div className="col-md-6">
         <img
-          className="w-100"
+          className="w-100 animate__animated animate__fadeInRight"
           style={{ maxHeight: "85%" }}
           src={imgURL}
           alt={imgURL.split("/")[imgURL.split("/").length - 1]}
