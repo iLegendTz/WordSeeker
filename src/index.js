db211c5a6bdecf39c85090f603a794e3547b4935
iLegendTz <miguel.isaac@live.com>
2021-11-24 03:41:38 -0600

    Se agregaron los estilos.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { SubtitlesApp } from "./SubtitlesApp";
+import "./styles/styles.scss";
 
 ReactDOM.render(<SubtitlesApp />, document.getElementById("root"));
