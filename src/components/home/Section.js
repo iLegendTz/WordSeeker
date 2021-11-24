e0c5e3f8dcc766e3822515e3c84f2f490b8bf434
iLegendTz <miguel.isaac@live.com>
2021-11-24 12:22:05 -0600

    Se implemento useOnScreen y ahora la animacion es al componente padre.

--- src/components/home/Section.js
+++ src/components/home/Section.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
+
+import { useOnScreen } from "../../hooks/useOnScreen";
 
 export const Section = ({ title, paragraphs, imgURL }) => {
+  const ref = useRef();
+  const onScreen = useOnScreen(ref);
+
   return (
-    <div className="section__container row">
+    <div
+      className={
+        onScreen
+          ? "section__container row animate__animated animate__fadeInLeft"
+          : "section__container row"
+      }
+      ref={ref}
+    >
       <div className="col-md-6">
-        <h2 className="display-4 mb-3 animate__animated animate__fadeInDown">
+        <h2 className="display-4 mb-3">
           <b>{title}</b>
         </h2>
         {paragraphs.map((paragraph, key) => {
@@ -18,7 +30,7 @@ export const Section = ({ title, paragraphs, imgURL }) => {
 
       <div className="col-md-6">
         <img
-          className="w-100 animate__animated animate__fadeInRight"
+          className="w-100"
           style={{ maxHeight: "85%" }}
           src={imgURL}
           alt={imgURL.split("/")[imgURL.split("/").length - 1]}
