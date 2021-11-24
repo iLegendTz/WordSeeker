493efa4c4360e6eac5689f3cdd2c635478d1d4e9
iLegendTz <miguel.isaac@live.com>
2021-11-24 04:32:03 -0600

    Se creo Navbar.

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light fixed-top"
+      style={{ backgroundColor: "white" }}
+    >
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  "nav-link " + (isActive && "active")
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  "nav-item nav-link " + (isActive && "active")
+                }
+                to="/"
+              >
+                New Project
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
