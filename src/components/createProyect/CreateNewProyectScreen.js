0637ea30c7754751b3a4dc0bda009ec92a0da1a2
iLegendTz <miguel.isaac@live.com>
2021-11-27 06:50:28 -0600

    CreateNewProyectScreen se hizo funcional.

--- src/components/createProyect/CreateNewProyectScreen.js
+++ src/components/createProyect/CreateNewProyectScreen.js
@@ -1,41 +1,62 @@
 import React, { useState } from "react";
 
+import { useAudioToText } from "../../hooks/useAudioToText";
+import { Spinner } from "../Spinner/Spinner";
+import { AudioPreview } from "./AudioPreview";
+
 export const CreateNewProyectScreen = () => {
-  const [audioFile, setAudioFile] = useState([]);
+  const [audioFile, setAudioFile] = useState(null);
+  const [formData, setFormData] = useState(null);
+  const { isLoading, text, getText } = useAudioToText();
 
-  const onFileUpload = (e) => {
-    setAudioFile([e.target.files[0]]);
+  const onFileUpload = async (e) => {
+    if (e.target.files.length > 0) {
+      setAudioFile(e.target.files[0]);
+
+      const data = new FormData();
+      data.append("audio", e.target.files[0]);
+      setFormData(data);
+    }
   };
-  return (
-    <div>
-      <h2 className="display-4 text-uppercase text-center">
-        <b>Selecciona un archivo de video o audio</b>
-      </h2>
-      <p className="text-center">Formatos validos: WAV, FLAC, MP3</p>
-      <form method="post" encType="multipart/form-data">
-        <div className="createNewProyect__icon-container text-center">
-          <label
-            htmlFor="audio"
-            className={
-              audioFile.length > 0
-                ? "fas fa-file-audio fa-10x createNewProyect__icon"
-                : "fas fa-folder-plus fa-10x createNewProyect__icon"
-            }
-          ></label>
-        </div>
-        <input
-          className="d-none"
-          accept=".wav,.flac,.mp3"
-          type="file"
-          name="audio"
-          id="audio"
-          onChange={onFileUpload}
-        />
 
-        <button type="submit" className="">
-          Aceptar
-        </button>
-      </form>
-    </div>
-  );
+  const audioToText = (e) => {
+    e.preventDefault();
+    getText(formData);
+  };
+
+  if (isLoading) return <Spinner />;
+  else if (text === "" || !text)
+    return (
+      <>
+        <h2 className="display-4 text-uppercase text-center">
+          <b>Selecciona un archivo de video o audio</b>
+        </h2>
+        <p className="text-center">Formatos validos: WAV, FLAC, MP3</p>
+        <form onSubmit={audioToText}>
+          <div className="text-center">
+            {audioFile ? (
+              <AudioPreview
+                audioFile={URL.createObjectURL(audioFile)}
+                isLoading={isLoading}
+              />
+            ) : (
+              <label
+                htmlFor="audio"
+                className="fas fa-folder-plus fa-10x createNewProyect__icon"
+                style={{ cursor: "pointer" }}
+              ></label>
+            )}
+          </div>
+          <input
+            className="d-none"
+            accept=".wav,.flac,.mp3"
+            type="file"
+            name="audio"
+            id="audio"
+            onChange={onFileUpload}
+          />
+        </form>
+      </>
+    );
+  else return <p>hola</p>;
 };
