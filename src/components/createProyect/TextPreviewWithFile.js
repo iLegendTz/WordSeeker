c9f1e9652c1bde487569fc0ae9fae78030651cda
iLegendTz <miguel.isaac@live.com>
2021-12-04 16:26:03 -0600

    Se creo TextPreviewWithFile.

---
+++ src/components/createProyect/TextPreviewWithFile.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { useCreateAndDownloadTextFile } from "../../hooks/useCreateAndDownloadTextFile";
+
+export const TextPreviewWithFile = ({ text, audioFile }) => {
+  const { onDownload } = useCreateAndDownloadTextFile();
+
+  return (
+    <div className="w-100 h-100">
+      <div className="w-100 h-75 createNewProyect__textPreview mb-4">
+        {text}
+      </div>
+      <div className="d-flex flex-row-reverse justify-content-around">
+        <button className="buttons__btn" onClick={() => onDownload(text)}>
+          Descargar
+        </button>
+
+        <audio controls className="mb-5">
+          <source src={audioFile} type="audio/ogg" />
+        </audio>
+      </div>
+    </div>
+  );
+};
