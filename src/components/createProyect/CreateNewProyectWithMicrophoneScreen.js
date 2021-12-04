2829eb4338531428a4842e0c7a71bf264f56d51e
iLegendTz <miguel.isaac@live.com>
2021-12-04 16:25:47 -0600

    Se creo CreateNewProyectWithMicrophoneScreen.

---
+++ src/components/createProyect/CreateNewProyectWithMicrophoneScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { TextPreviewWithMicrophone } from "./TextPreviewWithMicrophone";
+
+export const CreateNewProyectWithMicrophoneScreen = () => {
+  return <TextPreviewWithMicrophone />;
+};
