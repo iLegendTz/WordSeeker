5b07271be35f163d010809ceecb89bbdc02f56eb
iLegendTz <miguel.isaac@live.com>
2021-12-05 14:15:52 -0600

    Se creo Footer.

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-center">
+      <hr />
+
+      <p className="text-muted">
+        <i class="far fa-copyright"></i> Word Seeker
+      </p>
+    </div>
+  );
+};
