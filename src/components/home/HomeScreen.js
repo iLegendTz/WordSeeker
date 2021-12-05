ea20814ecbe398ab0d4041d5023d166d6cc4e861
iLegendTz <miguel.isaac@live.com>
2021-12-05 14:32:05 -0600

    Ahora se utiliza el componente CreateNewProjectOptions.

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -1,46 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+
+import { featuresData } from "../../data/featuresData";
+import { CreateNewProyectOptions } from "../createProyect/CreateNewProyectOptions";
 import { Feature } from "./Feature";
 
 export const HomeScreen = () => {
-  const navigate = useNavigate();
-  const options = { FILE: "File", MICROPHONE: "Microphone" };
-
-  const onOptionSelected = (option) => {
-    if (option === options.FILE) {
-      navigate(`newProject/file`);
-    } else {
-      navigate(`newProject/microphone`);
-    }
-  };
-
   return (
     <>
       <h2 className="display-4 text-uppercase text-center mb-4">
         <b>Transcribe el texto de tus audios</b>
       </h2>
 
-      <div className="row">
-        <div className="col-md-6 col-12 mb-5 d-flex justify-content-center">
-          <div
-            className="createNewProyect__optionContainer"
-            onClick={() => onOptionSelected(options.FILE)}
-          >
-            <i className="fas fa-folder-plus fa-10x icon__icon"></i>
-            <h3 className="text-dark">Subir un archivo</h3>
-          </div>
-        </div>
-
-        <div className="col-md-6 col-12 mb-5 d-flex justify-content-center">
-          <div
-            className="createNewProyect__optionContainer"
-            onClick={() => onOptionSelected(options.MICROPHONE)}
-          >
-            <i className="fas fa-microphone fa-10x icon__icon"></i>
-            <h3 className="text-dark">Usar mi microfono</h3>
-          </div>
-        </div>
-      </div>
+      <CreateNewProyectOptions />
 
       <div className="col-12 text-center mb-5">
         <h2>
@@ -58,23 +29,16 @@ export const HomeScreen = () => {
       </div>
 
       <div className="row">
-        <Feature
-          icon="fa-file-download"
-          title="Descarga Gratuita"
-          description="Transcripciones ilimitadas y descargas gratuitas."
-        />
-
-        <Feature
-          icon="fa-microphone-alt"
-          title="Archivo y Microfono"
-          description="La capacidad de poder usar tanto archivos de audio como tu microfono en tiempo real."
-        />
-
-        <Feature
-          icon="fa-laugh-beam"
-          title="Facil de Descargar"
-          description="El archivo con tu transcripcion a un solo click."
-        />
+        {featuresData.map(({ icon, title, description }, key) => {
+          return (
+            <Feature
+              key={key}
+              icon={icon}
+              title={title}
+              description={description}
+            />
+          );
+        })}
       </div>
     </>
   );
