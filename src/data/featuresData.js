9b0f6c51cd7510d3d14cd12bd4a3d97961c281af
iLegendTz <miguel.isaac@live.com>
2021-12-05 14:29:44 -0600

    Se creo featuresData.

---
+++ src/data/featuresData.js
@@ -0,0 +1,18 @@
+export const featuresData = [
+  {
+    icon: "fa-file-download",
+    title: "Descarga Gratuita",
+    description: "Transcripciones ilimitadas y descargas gratuitas.",
+  },
+  {
+    icon: "fa-microphone-alt",
+    title: "Archivo y Microfono",
+    description:
+      "La capacidad de poder usar tanto archivos de audio como tu microfono en tiempo real.",
+  },
+  {
+    icon: "fa-laugh-beam",
+    title: "Facil de Descargar",
+    description: "El archivo con tu transcripcion a un solo click.",
+  },
+];
