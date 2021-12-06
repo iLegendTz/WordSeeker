2d5096673813b358dc31ede0b40d2c0719bb63ce
iLegendTz <miguel.isaac@live.com>
2021-12-05 21:46:56 -0600

    Se creo admitedAudioMicLanguages.

---
+++ src/data/admitedAudioMicLanguages.js
@@ -0,0 +1,5 @@
+export const admitedAudioMicLanguages = [
+  { language: "English", languageCode: "en-US" },
+  { language: "Español", languageCode: "es-MX" },
+  { language: "Deutsch", languageCode: "de-DE" },
+];
