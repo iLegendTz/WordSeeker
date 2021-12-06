1d12c4ddcf2c30717cfd240884ea28302a203d75
iLegendTz <miguel.isaac@live.com>
2021-12-05 21:43:42 -0600

    Se agrego la ruta de FAQ.

--- src/router/PublicRoute.js
+++ src/router/PublicRoute.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { CreateNewProjectWithFile } from "../components/createProyect/CreateNewProjectWithFileScreen";
 import { CreateNewProjectWithMicrophoneScreen } from "../components/createProyect/CreateNewProjectWithMicrophoneScreen";
+import { FAQScreen } from "../components/FAQ/FAQScreen";
 import { HomeScreen } from "../components/home/HomeScreen";
 import { Footer } from "../components/ui/Footer";
 import { Navbar } from "../components/ui/Navbar";
@@ -16,13 +17,18 @@ export const PublicRoute = () => {
       <div style={{ flex: "1 1 auto" }}>
         <Routes>
           <Route index element={<HomeScreen />} />
+
           <Route path="newProject">
+            <Route index element={<Navigate to="/" />} />
             <Route path="file" element={<CreateNewProjectWithFile />} />
             <Route
               path="microphone"
               element={<CreateNewProjectWithMicrophoneScreen />}
             />
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
+
+          <Route path="FAQ" element={<FAQScreen />} />
         </Routes>
       </div>
 
