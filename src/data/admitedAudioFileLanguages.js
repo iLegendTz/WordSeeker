37ab288f60c4cbf5592c8c05a87b7253afee5a99
iLegendTz <miguel.isaac@live.com>
2021-12-05 21:46:44 -0600

    Se creo admitedAudioFileLanguages.

---
+++ src/data/admitedAudioFileLanguages.js
@@ -0,0 +1,7 @@
+export const admitedAudioFileLanguages = [
+  { language: "Inglés (Estados Unidos)", languageCode: "en-US" },
+  { language: "Inglés (Reino Unido)", languageCode: "en-GB" },
+  { language: "Español (España)", languageCode: "es-ES" },
+  { language: "Español (México)", languageCode: "es-MX" },
+  { language: "Japonés (Japón)", languageCode: "ja-JP" },
+];
