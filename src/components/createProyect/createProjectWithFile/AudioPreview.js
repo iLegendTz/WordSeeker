9ad2ec53a60f6192dc90fc451d1aad3980de6c12
iLegendTz <miguel.isaac@live.com>
2021-12-07 23:21:28 -0600

    Se elimino el selector de lenguage y el boton de seleccionar otro archivo ahora redirecciona.

--- src/components/createProyect/createProjectWithFile/AudioPreview.js
+++ src/components/createProyect/createProjectWithFile/AudioPreview.js
@@ -1,13 +1,6 @@
-import React, { useState } from "react";
-
-import { admitedAudioFileLanguages } from "../../../data/admitedAudioFileLanguages";
-import { LanguageSelector } from "../../ui/LanguageSelector";
+import React from "react";
 
 export const AudioPreview = ({ audioFile }) => {
-  const [language, setLanguage] = useState(
-    admitedAudioFileLanguages[0].languageCode
-  );
-
   return (
     <>
       <div className="col-12 text-center mb-2">
@@ -17,21 +10,16 @@ export const AudioPreview = ({ audioFile }) => {
         </audio>
       </div>
 
-      <div className="mb-5 col-12">
-        <div className="mx-auto" style={{ width: "300px" }}>
-          <LanguageSelector
-            languages={admitedAudioFileLanguages}
-            setLanguage={setLanguage}
-          />
-        </div>
-      </div>
-
       <div className="d-flex justify-content-around row">
-        <div className="buttons__btn buttons__white d-inline-block mb-2">
-          <label htmlFor="audio" className="mt-2">
-            Seleccionar otro archivo
-          </label>
-        </div>
+        <button
+          type="button"
+          onClick={() => {
+            window.location.reload();
+          }}
+          className="buttons__btn buttons__white mb-2"
+        >
+          Seleccionar otro archivo
+        </button>
 
         <button type="submit" className="buttons__btn">
           Aceptar
