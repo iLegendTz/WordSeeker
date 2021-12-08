715cc8a3ac425fc52c2892f2dcd9d91a59a37de4
iLegendTz <miguel.isaac@live.com>
2021-12-08 00:20:43 -0600

    Se agrego la variable fail.

--- src/hooks/useAudioToText.js
+++ src/hooks/useAudioToText.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 export const useAudioToText = () => {
+  const [fail, setFail] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [text, setText] = useState("");
 
   const getText = async (formData) => {
     setIsLoading(true);
+    // https://speechtotexttest.herokuapp.com/api/audio
     fetch("https://speechtotexttest.herokuapp.com/api/audio", {
       method: "POST",
       body: formData,
@@ -13,12 +15,21 @@ export const useAudioToText = () => {
       .then((response) => response.json())
       .then((data) => {
         setText(data.text);
+        if (data.fail) {
+          setFail(true);
+        } else {
+          setFail(false);
+        }
 
         setTimeout(() => {
           setIsLoading(false);
         }, 3000);
+      })
+      .catch(() => {
+        setFail(true);
+        setText("No se pudo completar la peticion, intente mas tarde.");
       });
   };
 
-  return { getText, text, isLoading };
+  return { getText, text, isLoading, fail };
 };
