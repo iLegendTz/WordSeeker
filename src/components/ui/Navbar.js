3f20fcca1f00277338c27437bc24fb233b66ec9e
iLegendTz <miguel.isaac@live.com>
2021-12-17 00:03:26 -0600

    Se quito el navbarBrand del navbar.

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -9,20 +9,6 @@ export const Navbar = () => {
       style={{ backgroundColor: "white" }}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Word Seeker
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
